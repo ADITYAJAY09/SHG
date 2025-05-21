08e212c26a7d40d6135ec6c2836db882604a1724
KUMAR ADITYA <167633894+ADITYAJAY09@users.noreply.github.com>
2025-05-22 04:25:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # MySQL Configuration
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Nicehome@1'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'shg_database'
 
 mysql = MySQL(app)
